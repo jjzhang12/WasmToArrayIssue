b6dbd15b436e1745fca464fbd713975f3d3340d9
JJ Zhang <53831675+jjzhang12@users.noreply.github.com>
2022-01-27 10:30:49 -0800

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+ArrayList list = new ArrayList();
+list.Add(Day.Monday);
+
+try
+{
+    var array = list.ToArray(typeof(Day));
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    return;
+}
+
+Console.WriteLine("OK");
+
+
+public enum Day
+{
+    Monday,
+    Tuesday
+}
